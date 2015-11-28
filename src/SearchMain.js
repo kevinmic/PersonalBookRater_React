21ea33b0d11aefdf921aca320dc37cc49e8e6dc9
Kevin Dodge <kdodge@alianza.com>
2015-11-28 11:52:32 -0700

    Moved Syles, starting search page

--- src/SearchMain.js
+++ src/SearchMain.js
@@ -2,11 +2,67 @@ import React from 'react';
 import { Link } from 'react-router';
 var PropTypes = React.PropTypes;
 
+var data = {
+  "key1": {
+    author: "Aaron, Finley",
+    seriesTitle: "Dragon Eye",
+    seriesBookNumber: 1,
+    title: "Dragon",
+    sexRating: "T",
+    swearRating: "B",
+    violenceRatign: "LV",
+    recommendRating: "4",
+    genre: "Fantasy",
+    imageUrl: "http://d.gr-assets.com/books/1417024668l/23620393.jpg",
+    locationOfBook: "Dons Kindle",
+    reviewDate: "",
+    review: "Ilsa doesn’t know who she is. She only knows her father left her in the care of a guy named Ram, who teaches her swordsmanship in a butcher shop until the day when it’s safe for her to come home. But it may never be safe, and their enemies are closing in. Ilsa and Ram are being hunted, and they must flee through the dangers that bar them from their homeland.  She is a Dragon but has’t learned that yet.  Some believe those ancient monsters don’t deserve to live.",
+  },
+  "key2": {
+    author: "Abramson, Traci Hunter",
+    title: "Smokescreen",
+    sexRating: "",
+    swearRating: "",
+    violenceRatign: "",
+    recommendRating: "8",
+    genre: "Fiction",
+    imageUrl: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRZ9ZtRc4zKk2-u4exKM9m1TLBgu1kjUS_PYf0P5yqNg3n_5Y12",
+    locationOfBook: "Audible",
+    reviewDate: "",
+    review: "Taylor a young female artist finds herself in the middle of an intrigue she cant understand;  a man was murdered in her hotel room and even when she gets back to the United States she finds that someone is stalking her.  Her boyfriend Quinn, the leader of a squad of navy Seals tries to unravel the mystery while protecting her against whatever and whoever."
+  },
+  "key3": {
+    author: "Beery, Catherine",
+    seriesTitle: "Facades",
+    seriesBookNumber: 2,
+    title: "Defender of the Empire",
+    sexRating: "T",
+    swearRating: "B",
+    violenceRatign: "FV",
+    recommendRating: "8",
+    genre: "",
+    imageUrl: "http://ecx.images-amazon.com/images/I/51RjCiygDjL._SX310_BO1,204,203,200_.jpg",
+    locationOfBook: "Kindle",
+    reviewDate: "",
+    review: "My name is Rylynn Sinclair, and I have survived the Admiral’s Challenge and my first month at the Legion Fleet Academy. I was even made a crewman, along with my friends, stationed on the Hail Mary.  Then my past caught up with me.  It all started with Ace showing up in my room talking with my roommate, and the letter he had. From that point on, everything in my life started to pull me back to Colony Lenti and the secrets that surrounded it."
+  }
+}
+
+      // <ol>
+      //   <li>Title</li>
+      //   <li>Author</li>
+      //   <li>Series</li>
+      //   <li>Overall Rating</li>
+      // </ol>
+
 var Search = React.createClass({
   render: function() {
     return (
       <div>
-        Search Main<br/>
+        <h2>Books</h2>
+        <div className={"row"}>
+          ROW
+        </div>
       </div>
     );
   }
--- src/addReview/AddReviewMain.js
+++ src/addReview/AddReviewMain.js
@@ -1,55 +1,21 @@
 import React from 'react';
+import Scales from '../const/ScaleConst';
 var PropTypes = React.PropTypes;
-
-const RATING_SCALE = [
-  {key:"1",  description:"I didnt like it, didnt finish it, and don't recommend it "},
-  {key:"2",  description:"I finished this book but didn't like it"},
-  {key:"3",  description:"I enjoyed it but not enough to to seek out other books by this author"},
-  {key:"4",  description:"It was OK, I might read another book in this series"},
-  {key:"5",  description:"I struggled to get caught up in the book but finally enjoyed it"},
-  {key:"6",  description:"I liked it but i was uncomfortable with some objectional elements (sex, language, graphic violence)"},
-  {key:"7",  description:"Conditional recommendation: I liked it enough to recommend it, but It has objectional content."},
-  {key:"8",  description:"Good Book"},
-  {key:"9",  description:"Full recommendation even though it has some objectional elements"},
-  {key:"10", description:"Full recommendation"}
-];
-
-const PROFANITY_SCALE = [
-  {key:"A", description:"No Profanity"},
-  {key:"B", description:"Ligh use of mild profanity"},
-  {key:"C", description:"Limited use of hard profanity"},
-  {key:"D", description:"One or more characters in the book tend to use hard profanity"},
-  {key:"P", description:"Profane, frequent and uncomfortable use of hard core profanity  "}
-];
-
-const SEXUAL_SCALE = [
-  {key:"G", description:"No offensive Sexual Content"},
-  {key:"T", description:"Sexual Tension or Sex Implied"},
-  {key:"PG", description:"Some undeswcribed Sexual Encounters"},
-  {key:"R", description:"Sexual Encounters described in detail with intent to tittilate"},
-];
-
-const VIOLENCE_SCALE = [
-  {key:"NV", description:"No or Little Violence"},
-  {key:"LV", description:"Some violent incidents not well described"},
-  {key:"FV", description:"Frequent violentincidents"},
-  {key:"VB", description:"Violent, bloody, or shocking, well described mayhem"},
-];
 
 const helpers = {
   ratingToOption: function(ratingList) {
     return ratingList.map((rating) => {
       const {key, description} = rating;
-      return <option key={key}>{key + '-' + description}</option>;
+      return <option key={key}>{key + ' - ' + description}</option>;
     });
   }
 }
 
 var RatingOptions = React.createClass({
   propTypes: {
-    rateList: React.PropTypes.array,
-    id : React.PropTypes.string,
-    label : React.PropTypes.string
+    rateList: React.PropTypes.arrayOf(Scales.SCALE_PROPTYPE).isRequired,
+    id : React.PropTypes.string.isRequired,
+    label : React.PropTypes.string.isRequired
   },
   render: function() {
     return (
@@ -57,6 +23,7 @@ var RatingOptions = React.createClass({
           <label htmlFor={this.props.id}>{this.props.label}:</label>
           <br />
           <select id={this.props.id}>
+            <option>Please Select</option>
             {helpers.ratingToOption(this.props.rateList)}
           </select>
         </div>
@@ -77,13 +44,38 @@ var AddReviewMain = React.createClass({
             <input type="text" className="form-control" id="title"/>
           </div>
           <div className="form-group">
+            <label htmlFor="series">Series: </label>
+            <input type="text" className="form-control" id="series"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="bookNum">Book Number: </label>
+            <input type="text" className="form-control" id="bookNum"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="imageUrl">Image Url: </label>
+            <input type="text" className="form-control" id="imageUrl"/>
+          </div>
+          <div className="form-group">
             <label htmlFor="author">Author: </label>
             <input type="text" className="form-control" id="author"/>
           </div>
-          <RatingOptions id="overallRating" label="Overall Rating" rateList={RATING_SCALE}/>
-          <RatingOptions id="profanityRating" label="Profanity Rating" rateList={PROFANITY_SCALE}/>
-          <RatingOptions id="sexualRating" label="Sexual Rating" rateList={SEXUAL_SCALE}/>
-          <RatingOptions id="violenceRating" label="Violence Rating" rateList={VIOLENCE_SCALE}/>
+          <div className="form-group">
+            <label htmlFor="genre">Genre: </label>
+            <input type="text" className="form-control" id="genre"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="bookLocation">Location of Book: </label>
+            <input type="text" className="form-control" id="bookLocation"/>
+          </div>
+          <RatingOptions id="overallRating" label="Overall Rating" rateList={Scales.RATING_SCALE}/>
+          <RatingOptions id="profanityRating" label="Profanity Rating" rateList={Scales.PROFANITY_SCALE}/>
+          <RatingOptions id="sexualRating" label="Sexual Rating" rateList={Scales.SEXUAL_SCALE}/>
+          <RatingOptions id="violenceRating" label="Violence Rating" rateList={Scales.VIOLENCE_SCALE}/>
+          <div className="form-group">
+            <label htmlFor="reviewDescription">Review: </label>
+            <textarea className="form-control" rows="5" id="reviewDescrption"/>
+          </div>
+
           <button type="submit" className="btn btn-default">Submit</button>
         </form>
       </div>
