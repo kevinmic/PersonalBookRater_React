e95b906722f5e4bab45d1833662e768295ed4d3f
Kevin Dodge <kdodge@alianza.com>
2015-12-04 23:49:36 -0700

    Understanding and cleaning house

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,11 @@ module.exports = {
       {
         test: /\.css$/,
         loader: "style-loader!css-loader"
-      }
-  ]
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+    ]
   }
 };
