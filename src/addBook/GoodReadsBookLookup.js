e494db25437e7da9d568efeec5ec8cf743df0cae
Kevin Dodge <kdodge@alianza.com>
2018-10-24 23:35:48 -0600

    Fixing GoodReads

--- src/addBook/GoodReadsBookLookup.js
+++ src/addBook/GoodReadsBookLookup.js
@@ -11,94 +11,17 @@ var loadUrl = function() {
 
 loadUrl();
 
-var seriesPatternList = [
-  /^(.+)\s+\((.+)[,:;]\s*\#(\d+)\)$/,
-  /^(.+)\s+\((.+)[,:;]\s*Book\s+(\d+)\)$/,
-  /^(.+)\s+\((.+)[,:;]\s*Book\s+(One|Two|Three|Four|Five|Six|Seven|Eight|Nine)\)$/,
-  /^(.+)\s+\((.+)\s+Book\s+(\d+)\)$/,
-  /^(.+)\s+\((.+)\s*\#(\d+)\)$/,
-];
-
-var parseTitle = function(title) {
-  if (!_.isString(title)) {
-    title = title.__cdata;
-  }
-  for (var i = 0; i < seriesPatternList.length; i++) {
-    var regex = seriesPatternList[i];
-    var match = title.match(regex);
-    if (match) {
-      // console.log("MATCH", regex, title);
-      return {title: match[1], seriesTitle:match[2], seriesBookNumber:match[3]}
-    }
-  }
-  return {title: title, seriesTitle:"", seriesBookNumber:""}
-};
-
-var parseAuthor = function(author) {
-  var index = author.name.lastIndexOf(" ");
-  return {author: author.name.slice(index + 1, author.name.length) + ", " + author.name.slice(0, index)};
-};
-
-var descriptionPatternList = [
-  {tag: "br", replace:"\n"},
-  {tag: "em", replace:"_"},
-  {tag: "strong",replace:"__"},
-];
-
-var parseDescription = function(descr) {
-  if (!_.isString(descr)) {
-    descr = descr.__cdata;
-  }
-  for (var i = 0; i < descriptionPatternList.length; i++) {
-    var pat = descriptionPatternList[i];
-    descr = descr.replace(new RegExp("\<" + pat.tag + "\>", "gi"), pat.replace);
-    descr = descr.replace(new RegExp("\<" + pat.tag + "\/\>", "gi"), pat.replace);
-    descr = descr.replace(new RegExp("\<\/" + pat.tag + "\>", "gi"), pat.replace);
-  }
-  return descr + "\n<sub><sup>(Description From GoodReads.com)</sup></sub>";
-}
-
-var parseImage = function(image) {
-  if (image) {
-    if (image.indexOf("nophoto") > 0) {
-      image = "";
-    }
-    else {
-      var index = image.lastIndexOf("s");
-      // I want the medium sized image
-      image = image.slice(0, index) + "m" + image.slice(index + 1);
-    }
-  }
-  return image;
-};
-
 var lookupBook = function(grBookId, callback) {
   if (!WEBTASK_URL) {
       callback("Goodreads URL not loaded from Firebase, contact Kevin", null);
       return;
   }
 
-  var url = WEBTASK_URL + "&type=book&bookId=" + grBookId;
+  var url = WEBTASK_URL + "?type=book&bookId=" + grBookId;
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", (param) => {
     if (oReq.status == 200) {
-      var response = oReq.response;
-
-      var resObj = x2js.xml_str2json(response);
-      if (resObj.GoodreadsResponse && resObj.GoodreadsResponse.book) {
-        var grBook = resObj.GoodreadsResponse.book;
-        // console.log("GoodreadsBook", grBook);
-        var book = {
-          ...parseTitle(grBook.title),
-          ...parseAuthor(grBook.authors.author),
-          imageUrl:parseImage(grBook.small_image_url),
-          goodreadsId: grBook.id,
-          synopsis: parseDescription(grBook.description),
-        };
-
-        callback(null, book);
-      }
-
+      callback(null, JSON.parse(oReq.response));
     }
     else {
       callback("Error querying url, contact Kevin - " + url, null);
@@ -114,35 +37,11 @@ var searchBook = function(name, callback) {
       return;
   }
 
-  var url = WEBTASK_URL + "&type=search&q=" + name;
+  var url = WEBTASK_URL + "?type=search&q=" + name;
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", (param) => {
     if (oReq.status == 200) {
-      var response = oReq.response;
-
-      var search = x2js.xml_str2json(response);
-
-      var books;
-      if (search.GoodreadsResponse.search.results.work && search.GoodreadsResponse.search.results.work.length > 0) {
-        books = search.GoodreadsResponse.search.results.work
-          .map((book) => book.best_book)
-          .map((book) => {
-              // console.log(book);
-            var {title, author, small_image_url, id} = book
-
-            return {
-              ...parseTitle(title),
-              ...parseAuthor(author),
-              imageUrl:parseImage(small_image_url),
-              goodreadsId: id.__text,
-            };
-        });
-      }
-      else {
-        books = [];
-      }
-
-      callback(null, books);
+      callback(null, JSON.parse(oReq.response));
     }
     else {
       callback("Error querying url, contact Kevin - " + url, null);
